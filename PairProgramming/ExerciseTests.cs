270cb4994d841299ecd59c064e95814111e6ae97
Rui Rosa Mendes <ruirosamendes@gmail.com>
2015-08-28 18:15:54 +0100

    unit test setup

---
+++ PairProgramming/ExerciseTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairProgramming
+{
+    [TestFixture]
+    public class ExerciseTests
+    {
+
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+
+    
+
+}
